db605e48e74e2c7364d27c7d9d8ceb1fef8ec7e7
JuanBravo <juanbravo@JuanBravos-MBP.home>
2023-01-28 14:38:20 +0200

    Fetch office data and print json

--- web/src/pages/LocationsPage/LocationsPage.tsx
+++ web/src/pages/LocationsPage/LocationsPage.tsx
@@ -1,11 +1,14 @@
 import { MetaTags } from '@redwoodjs/web'
 
+import OfficesCell from 'src/components/OfficesCell'
+
 const LocationsPage = () => {
   return (
     <>
       <MetaTags title="Locations" description="Locations page" />
 
       <h1>LocationsPage</h1>
+      <OfficesCell />
     </>
   )
 }
