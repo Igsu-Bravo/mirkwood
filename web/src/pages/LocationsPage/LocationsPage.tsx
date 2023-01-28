88faca3f13a2b209393b623601becd21dc928790
JuanBravo <juanbravo@JuanBravos-MBP.home>
2023-01-28 14:33:56 +0200

    Remove boilerplate

--- web/src/pages/LocationsPage/LocationsPage.tsx
+++ web/src/pages/LocationsPage/LocationsPage.tsx
@@ -1,4 +1,3 @@
-import { Link, routes } from '@redwoodjs/router'
 import { MetaTags } from '@redwoodjs/web'
 
 const LocationsPage = () => {
@@ -7,13 +6,6 @@ const LocationsPage = () => {
       <MetaTags title="Locations" description="Locations page" />
 
       <h1>LocationsPage</h1>
-      <p>
-        Find me in <code>./web/src/pages/LocationsPage/LocationsPage.tsx</code>
-      </p>
-      <p>
-        My default route is named <code>locations</code>, link to me with `
-        <Link to={routes.locations()}>Locations</Link>`
-      </p>
     </>
   )
 }
